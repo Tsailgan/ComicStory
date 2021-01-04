78c58153e26b2c772e85078d3b735b05d3e2ec2f
Tsailgan <Tsailgan>
2021-01-05 02:07:10 +0800

    fix

--- loading.js
+++ loading.js
@@ -1,21 +1,21 @@
-// $("#progressFill").animate({ 
-//     width: "100%"
-//   }, 7.5*1000);
+$("#progressFill").animate({ 
+    width: "100%"
+  }, 7.5*1000);
 
-// $("#percentage").animate({ 
-//     left: "100%"
-// }, 7.5*1000);
+$("#percentage").animate({ 
+    left: "100%"
+}, 7.5*1000);
 
-// var count = 0;
-// var timer = setInterval(function(){
-//     count++;
-//     var percentageValue = count + '%'
-//     $("#percentage_number").html(percentageValue);
-//     if(count >= 100) {
-//         clearInterval(timer);
-//         window.location.href="cartoon.html"
-//     }
-//     },75)
+var count = 0;
+var timer = setInterval(function(){
+    count++;
+    var percentageValue = count + '%'
+    $("#percentage_number").html(percentageValue);
+    if(count >= 100) {
+        clearInterval(timer);
+        window.location.href="cartoon.html"
+    }
+    },75)
 
 !function(){
     var myHeight=window.innerHeight;
